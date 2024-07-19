7a8f8295a4ab71266ec97b13de447895c378b909
ritesh <riteshchaudhary375@gmail.com>
2024-07-19 22:41:25 +0545

    add a middleware and a function to handle errors

--- api/controllers/auth.controller.js
+++ api/controllers/auth.controller.js
@@ -1,9 +1,10 @@
 import User from "../models/user.model.js";
 import bcryptjs from "bcryptjs";
+import { errorHandler } from "../utils/error.js";
 
 const salt = process.env.SALT_ROUNDS;
 
-export const signup = async (req, res) => {
+export const signup = async (req, res, next) => {
   //   res.json({ message: "Auth route" });
   //   console.log(req.body);
 
@@ -17,9 +18,11 @@ export const signup = async (req, res) => {
     email === "" ||
     password === ""
   ) {
-    return res
+    /* return res
       .status(400)
-      .json({ status: false, message: "All fields are required" });
+      .json({ status: false, message: "All fields are required" }); */
+
+    next(errorHandler(400, "All filds are required"));
   }
 
   const hashedPassword = bcryptjs.hashSync(password, salt);
@@ -37,9 +40,11 @@ export const signup = async (req, res) => {
       message: "Signup successful",
     });
   } catch (error) {
-    res.status(500).json({
+    /* res.status(500).json({
       success: false,
       message: error.message,
-    });
+    }); */
+
+    next(error);
   }
 };
--- api/index.js
+++ api/index.js
@@ -26,3 +26,15 @@ app.listen(process.env.PORT, () => {
 // API Test
 app.use("/api/user", userRoutes);
 app.use("/api/auth", authRoutes);
+
+// MIDDLEWARE FOR ERROR
+app.use((err, req, res, next) => {
+  const statusCode = err.statusCode || 500;
+  const message = err.message || "Internal Server Error";
+
+  res.status(statusCode).json({
+    success: false,
+    statusCode,
+    message,
+  });
+});
