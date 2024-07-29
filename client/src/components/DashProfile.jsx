00e6623fcc0d10ba3e9bb8db8315f75ea34086cb
ritesh <riteshchaudhary375@gmail.com>
2024-07-29 18:13:32 +0545

    complete create a post page ui

--- client/src/components/DashProfile.jsx
+++ client/src/components/DashProfile.jsx
@@ -23,8 +23,10 @@ import {
 
 import { HiOutlineExclamationCircle } from "react-icons/hi";
 
+import { Link } from "react-router-dom";
+
 const DashProfile = () => {
-  const { currentUser, error } = useSelector((state) => state.user);
+  const { currentUser, error, loading } = useSelector((state) => state.user);
   const [imageFile, setImageFile] = useState(null);
   const [imageFileUrl, setImageFileUrl] = useState(null);
   const [imageFileUploadProgress, setImageFileUploadProgress] = useState(null);
@@ -262,9 +264,28 @@ const DashProfile = () => {
           placeholder="password"
           onChange={handleChange}
         />
-        <Button type="submit" gradientDuoTone="purpleToBlue" outline>
-          Update
+        <Button
+          disabled={loading || imageFileUploading}
+          type="submit"
+          gradientDuoTone="purpleToBlue"
+          outline
+        >
+          {/* Update */}
+          {loading ? "Loading..." : "Update"}
         </Button>
+
+        {/* Create Post button for admin user only */}
+        {currentUser.isAdmin && (
+          <Link to={"/create-post"}>
+            <Button
+              type="button"
+              gradientDuoTone="purpleToPink"
+              className="w-full"
+            >
+              Create a post
+            </Button>
+          </Link>
+        )}
       </form>
       <div className="text-red-500 flex justify-between mt-5">
         <span className="cursor-pointer" onClick={() => setShowModal(true)}>
