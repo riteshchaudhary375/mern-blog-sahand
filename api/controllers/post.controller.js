c72bca91c996717b8298fe717666e352c18d43c5
ritesh <riteshchaudhary375@gmail.com>
2024-07-30 22:09:27 +0545

    create get posts api route

--- api/controllers/post.controller.js
+++ api/controllers/post.controller.js
@@ -1,3 +1,4 @@
+import { now } from "mongoose";
 import Post from "../models/post.model.js";
 import { errorHandler } from "../utils/error.js";
 
@@ -28,6 +29,56 @@ export const create = async (req, res, next) => {
     // console.log(savedPost);
 
     res.status(201).json(savedPost);
+  } catch (error) {
+    next(error);
+  }
+};
+
+export const getPosts = async (req, res, next) => {
+  try {
+    const startIndex = parseInt(req.query.startIndex) || 0;
+    const limit = parseInt(req.query.limit) || 9;
+
+    // 'asc' => ascending
+    // 1 for ascending and -1 for descending in Mongodb
+    const sortDirection = req.query.order === "asc" ? 1 : -1;
+    const posts = await Post.find({
+      ...(req.query.userId && { userId: req.query.userId }),
+      ...(req.query.category && { category: req.query.category }),
+      ...(req.query.slug && { category: req.query.slug }),
+      ...(req.query.postId && { _id: req.query.postId }),
+      ...(req.query.searchTerm && {
+        $or: [
+          { title: { $regex: req.query.searchTerm, $options: "i" } },
+          { content: { $regex: req.query.searchTerm, $options: "i" } },
+        ],
+      }),
+    })
+      .sort({ updateAt: sortDirection })
+      .skip(startIndex)
+      .limit(limit);
+
+    // for total no of posts
+    const totalPosts = await Post.countDocuments();
+
+    // total posts  of last months
+    const now = new Date();
+    const oneMonthAgo = new Date(
+      now.getFullYear(),
+      now.getMonth() - 1,
+      now.getDate()
+    );
+
+    const lastMonthPosts = await Post.countDocuments({
+      createdAt: { $gte: oneMonthAgo },
+    });
+
+    res.status(200).json({
+      posts,
+      totalPosts,
+      lastMonthPosts,
+    });
+
   } catch (error) {
     next(error);
   }
